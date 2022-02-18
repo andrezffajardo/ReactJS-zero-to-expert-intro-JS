4844ce44aa0d86ecbd0fa7b75a01f06d04b941da
Andres F. Fajardo <andrezffajardo@gmail.com>
2022-02-18 16:32:22 -0500

    Intro-JS Example Const and Let

---
+++ src/basics/01-const-let.js
@@ -0,0 +1,18 @@
+
+const name = 'Andrew';
+const lastName = 'Fajardo';
+
+let value = 6;
+
+value = 4;
+
+console.log( name, lastName, value );
+
+if ( true ) {
+    let value = 8;
+    console.log( value );
+}
+
+console.log( value );
+
+
