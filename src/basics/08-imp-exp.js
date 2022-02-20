3a01392f707d482cd9f192d0c3006b1d134b71f9
Andres F. Fajardo <andrezffajardo@gmail.com>
2022-02-20 18:34:20 -0500

    Intro-JS Example import and export correction

--- src/basics/08-imp-exp.js
+++ src/basics/08-imp-exp.js
@@ -1,9 +1,9 @@
 //import { heroes } from './data/heroes';
 import heroes, { owners } from '../data/heroes';
 
-console.log( owners );
-
-console.log( heroes );
+// console.log( owners );
+//
+// console.log( heroes );
 
 // const getHeroeById = (id) => {
 //     return heroes.find( ( heroe ) => {
@@ -14,10 +14,10 @@ console.log( heroes );
 //         }
 //     });
 // }
-const getHeroeById = (id) => heroes.find( ( heroe ) => heroe.id === id );
+export const getHeroeById = (id) => heroes.find( ( heroe ) => heroe.id === id );
 
-console.log( getHeroeById(2) );
+//console.log( getHeroeById(2) );
 
-const getHeroesByOwner = ( owner ) => heroes.filter( ( heroe ) => heroe.owner === owner );
+export const getHeroesByOwner = ( owner ) => heroes.filter( ( heroe ) => heroe.owner === owner );
 
-console.log( getHeroesByOwner('Marvel') );
+//console.log( getHeroesByOwner('Marvel') );
