ee8c795d3efa42ab9b3afa87019cf86f9307e631
Andres F. Fajardo <andrezffajardo@gmail.com>
2022-02-20 20:35:59 -0500

    Intro-JS Example Async-Await

---
+++ src/basics/11-Async-Await.js
@@ -0,0 +1,22 @@
+
+const getImage = async() => {
+
+    try {
+        const apiKey = 'NFmvEDyd0WMPStwnG0PZTbDGVSk2BQJL';
+        const resp = await fetch(`https://api.giphy.com/v1/gifs/random?api_key=${ apiKey }`);
+        const {data} = await resp.json();
+
+        const { url } = data.images.original;
+        const img = document.createElement('img');
+        img.src = url;
+
+        document.body.append(img);
+    } catch (error) {
+        console.error(error);
+    }
+
+
+
+}
+
+getImage();
